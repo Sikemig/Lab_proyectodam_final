9da533c0524dabfc33fa57e04f10981b462547c3
Sikem Iglesias <sikemig@gmail.com>
2024-04-26 16:40:55 +0200

    user2 prepara testing de la calculadora

--- src/testing/TestCalculadora.java
+++ src/testing/TestCalculadora.java
@@ -1,5 +1,17 @@
 package testing;
 
+import dao.CalculadoraDao;
+import dao.CalculadoraDaoImpl;
+
 public class TestCalculadora {
 
+	public static void main(String[] args) {
+		CalculadoraDao cdao = new CalculadoraDaoImpl();
+		
+		System.out.println(cdao.sumar(20,5));
+		System.out.println(cdao.restar(20,5));
+		System.out.println(cdao.multiplicar(20,5));
+		System.out.println(cdao.dividir(20,5));
+		System.out.println(cdao.resto(20,5));
+	}
 }
